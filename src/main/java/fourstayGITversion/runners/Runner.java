96c0134ea68a3af8949344ff44d7ec4183b83766
anny1310 <anny1310@mail.ru>
2017-08-29 12:28:05 -0400

    Successful Import and push

--- src/main/java/fourstayGITversion/runners/Runner.java
+++ src/main/java/fourstayGITversion/runners/Runner.java
@@ -14,6 +14,7 @@ import cucumber.api.junit.Cucumber;
 		tags="@Regression",
 		dryRun=false
 		)
+	// I, Anna, have successfully imported git project and I am now committing and pushing this message.
 public class Runner {
 
 	
